94835f1982772e70bd2368b2e779de97702059a6
thrugl <thorarinn.bjorn@gmail.com>
2020-09-25 00:15:48 +0000

    foo

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
 module.exports = {
-	configureWebpack: {
-		resolve: {
-      alias: {
-				'@': path.resolve(__dirname, './src/'),
-				'@@': path.resolve(__dirname, './src/components/'),
-				'%': path.resolve(__dirname, './src/assets/'),
-				'#': path.resolve(__dirname, './src/composables/')
-      }
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+			resolve: {
+				alias: {
+					'@': path.resolve(__dirname, './src/'),
+					'@@': path.resolve(__dirname, './src/components/'),
+					'%': path.resolve(__dirname, './src/assets/'),
+					'#': path.resolve(__dirname, './src/composables/')
+				}
+			},
+      plugins: [
+        new PrerenderSPAPlugin(
+          path.resolve(__dirname, 'dist'),
+          [ '/']
+        ),
+      ]
     }
 	},
   lintOnSave: false
